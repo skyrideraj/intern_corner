4eb4374dac7f35cd8bfd5fbefe4930b52e940db0
inuishan <ishanjain1991@gmail.com>
2013-03-14 02:43:05 +0530

    adds student, login and user class

---
+++ php/classes/Student.php
@@ -0,0 +1,29 @@
+<?
+require_once 'User.php';
+/**
+* 
+*/
+class Student extends User
+{
+	var field;
+	var cpi;
+	var batch;
+	var reputation;
+	var profile_complete;
+	var professor_rating_average;
+	var resume;
+	var experience;
+	function professorRate(){
+
+	}
+	function displayProfile(){
+
+	}
+	
+	function __construct(field,cpi,batch,reputation,profile_complete,professor_rating_average,resume,experience)
+	{
+		# code...
+
+	}
+}
+?>
